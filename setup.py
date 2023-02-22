fea9ac351aafd6de879c902789205e96b505e738
smith <smith@bdb.ai>
2023-02-22 11:19:21 +0530

    First_push.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
     README = readme.read()
 
 setuptools.setup(
-    name="util_package",
+    name="utilbdb",
     version="0.0.1",
     author="smith",
     author_email="smith@bdb.ai",
@@ -18,7 +18,7 @@ setuptools.setup(
     url="https://github.com/SmithSaurav/util_package",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: BDB License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
